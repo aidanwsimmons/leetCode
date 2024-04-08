eb8b66e4f476c1ed7d9e163c4620bb9a3ffff436
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-04-08 12:41:48 -0700

    #1493 sliding window solution

---
+++ longestSubarrayOf1sAfterDeletingOneElement.js
@@ -0,0 +1,50 @@
+// Given a binary array nums, you should delete one element from it.
+
+// Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,0,1]
+// Output: 3
+// Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+// Example 2:
+
+// Input: nums = [0,1,1,1,0,1,1,0,1]
+// Output: 5
+// Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+// Example 3:
+
+// Input: nums = [1,1,1]
+// Output: 2
+// Explanation: You must delete one element.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// nums[i] is either 0 or 1.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function(nums) {
+    res = 0
+    let start = 0;
+    let zeros = 0
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === 0) zeros++
+        while(zeros > 1){
+            if(nums[start] === 0){
+                zeros--
+            }
+            start++
+        }
+        res = Math.max(res, i - start)
+    }
+
+    return res
+};
