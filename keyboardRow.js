a9f1a0ecca195b893dbe09832e882e611300e7df
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-04-29 11:13:17 -0700

    #500 string solution

---
+++ keyboardRow.js
@@ -0,0 +1,61 @@
+// Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard like the image below.
+
+// In the American keyboard:
+
+// the first row consists of the characters "qwertyuiop",
+// the second row consists of the characters "asdfghjkl", and
+// the third row consists of the characters "zxcvbnm".
+
+ 
+
+// Example 1:
+
+// Input: words = ["Hello","Alaska","Dad","Peace"]
+// Output: ["Alaska","Dad"]
+// Example 2:
+
+// Input: words = ["omk"]
+// Output: []
+// Example 3:
+
+// Input: words = ["adsdf","sfd"]
+// Output: ["adsdf","sfd"]
+ 
+
+// Constraints:
+
+// 1 <= words.length <= 20
+// 1 <= words[i].length <= 100
+// words[i] consists of English letters (both lowercase and uppercase). 
+
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(words) {
+    const top = 'qwertyuiop'
+    const mid = 'asdfghjkl'
+    const bot = 'zxcvbnm'
+
+    let validWords = []
+
+    function allSameRow(arr){
+        if(arr.every(el => top.indexOf(el) !== -1)){
+            return true
+        }
+        else if(arr.every(el => mid.indexOf(el) !== -1)){
+            return true
+        }
+        else if(arr.every(el => bot.indexOf(el) !== -1)){
+            return true
+        }
+        else return false
+    }
+
+    words.forEach(word => {
+        let copy = word.toLowerCase().split('')
+        if(allSameRow(copy)) validWords.push(word)
+    })
+
+    return validWords
+};
