462d9c6e39fc760478edfa9f34eefcca6baa0417
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-05-02 14:31:20 -0700

    improved solution

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -42,3 +42,26 @@ var groupAnagrams = function(strs) {
 
     return Object.values(map)
 };
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map = {}
+
+    for(let str of strs){
+        let count = new Array(26).fill(0)
+
+        for(let char of str){
+            count[char.charCodeAt(0) - 'a'.charCodeAt(0)]++
+        }
+        let key = count.join('-')
+        if(!map[key]){
+            map[key] = []
+        }
+        map[key].push(str)
+    }
+
+    return Object.values(map)
+};
